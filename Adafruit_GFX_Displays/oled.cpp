c2bd24083353848e37cb35f2de504d575391de84
Michael L. Mehr <mike@azuresults.com>
2016-09-02 12:36:28 -0700

    Fixed bugs by using static 1306 driver object in library instead of heap allocation. Project runs properly now.

--- Adafruit_GFX_Displays/oled.cpp
+++ Adafruit_GFX_Displays/oled.cpp
@@ -10,17 +10,12 @@
 #include "oled.h"
 #include "Adafruit_SSD1306.h"
 
-Adafruit_SSD1306 *pOLED = NULL;
+Adafruit_SSD1306 OLED(OLED_DC, OLED_RST, OLED_CS);
 
 // set up the pins on the LPC-1769 via the class into the SPI implementation (doesn't work for I2C for now)
 // will return 0 if cannot instantiate the class, 1 if it's good to go
 int oled_init(int DC, int RST, int CS, int reset) {
-	if (!pOLED)
-		pOLED = new Adafruit_SSD1306(DC, RST, CS);
-	if (pOLED) {
-		// call begin() here
-		return 1;
-	}
+	// perform the reset if asked, return result
 	return 0;
 }
 
